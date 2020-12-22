720e1d1847ea4132b020fc3496af9a68eee38280
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2020-12-22 08:46:27 -0300

    Ask for user input

--- src/files_accessor.py
+++ src/files_accessor.py
@@ -1,6 +1,7 @@
+from hmac import compare_digest as compare_hash
 from pathlib import Path
-from colors import red
 from crypt import crypt
+from colors import red
 
 SAFENOTES_DIR_PATH = Path('~/.config/Safenotes/').expanduser()
 PASSWORD_FILE_PATH = str(SAFENOTES_DIR_PATH / 'password')
@@ -25,10 +26,21 @@ def define_user_password() -> None:
     Defines a password for the user.
     Caller must decide whether this really must be done.
     """
-    print(red('It seems you do not have a password yet.'))
-    print('Please, define it below:')
-
     password = input('>>>')
     hashed_passwd = crypt(password)
     with open(PASSWORD_FILE_PATH, 'w') as f:
         f.write(hashed_passwd)
+
+
+def load_user_password() -> None:
+    """
+    Prompts for password until user gets it right (i.e. hashes match)
+    """
+    hashed_passwd = open(PASSWORD_FILE_PATH, 'r').read()
+    print('Please, type in your password to have access to your notes:')
+    password_typed = input('>>>')
+    while not compare_hash(hashed_passwd, crypt(password_typed, hashed_passwd)):
+        print(red('Wrong password. Please, try again.'))
+        password_typed = input('>>>')
+
+    return password_typed
--- src/safenotes.py
+++ src/safenotes.py
@@ -1,3 +1,4 @@
+from colors import red
 import files_accessor
 
 
@@ -5,9 +6,11 @@ def main():
     files_accessor.ensure_safenotes_dir_exists()
 
     if not files_accessor.password_file_exists():
+        print(red('It seems you do not have a password yet.'))
+        print('Please, define it below:')
         files_accessor.define_user_password()
-    else:
-        print('You have a password!')
+
+    print(files_accessor.load_user_password())
 
 
 if __name__ == "__main__":
